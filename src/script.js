2f9cfd24ad081ef48a01a3b93c9fa5b65c5d0fa4
2biazdk <tobias@2biaz.dk>
2017-09-04 21:39:03 +0200

    Don't replace the very first character in a line
    For non-diff lines, Github will insert a space instead of a plus or minus, which should not be replaced.

--- src/script.js
+++ src/script.js
@@ -31,24 +31,24 @@
 			if (child.nodeName !== "#text") {
 				visualizeWSChildren(child, whitespace);
 				return;
-			} else {
-				var newChild = document.createElement('span');
-
-				var html = htmlEntities(child.nodeValue);
-				html = html.replace(whitespace.regex, '<span class="_visualizeWS-' + whitespace.name + '">' + whitespace.char + '</span>');
-
-				newChild.innerHTML = html;
-
-				child.parentNode.insertBefore(newChild, child);
-				child.remove();
 			}
+
+			var newChild = document.createElement("span");
+			var className = "_visualizeWS _visualizeWS-" + whitespace.name;
+
+			var html = htmlEntities(child.nodeValue);
+			html = html.replace(whitespace.regex, '<span class="_visualizeWS _visualizeWS-' + whitespace.name + '">' + whitespace.char + '</span>');
+
+			newChild.innerHTML = html;
+
+			child.parentNode.insertBefore(newChild, child);
+			child.remove();
 		});
 	}
 
 	function htmlEntities(string) {
-		return String(string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+		return String(string).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
 	}
 })();
 
-/* TODO: Don't replace the very first character in a line. (For non-diff lines, Github will insert a space instead of a plus or minus.) */
 /* TODO: Make sure extension is run on each page load, when clicking around on a github pull request. */
